5a63038db58699d7347ba693533cffdaa78cc1b2
YUGAL <yugal.kishore.sinha@gmail.com>
2018-11-20 10:43:45 -0500

    My Django app, first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yugal.kishore.sinha\desktop\python\django\girl\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
